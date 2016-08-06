07feef9dd81a72a222916ca5096150aef774bdf2
Matt Pindell <pindell.m@gmail.com>
2016-08-06 17:46:41 -0600

    CsvParser creates customers, merchants, and items

--- sales_admin_submission/app/models/csv_parser.rb
+++ sales_admin_submission/app/models/csv_parser.rb
@@ -7,11 +7,28 @@ class CsvParser
   def parse_by_row(csv)
     CSV.foreach(csv, headers: true, header_converters: :symbol) do |row|
       customer = customer_parser(row)
+      merchant = merchant_parser(row)
+      item     = item_parser(row, merchant.id)
     end
   end
 
   def customer_parser(data)
     Customer.create(name: data[:customer_name])
+  end
+
+  def merchant_parser(data)
+    Merchant.create(
+      name:    data[:merchant_name],
+      address: data[:merchant_address]
+    )
+  end
+
+  def item_parser(data, merchant_id)
+    Item.create(
+      description: data[:item_description],
+      price:       data[:item_price],
+      merchant_id: merchant_id
+    )
   end
 
 end
