189345ef5f69c4cc87777da6149acc50f7fde810
Matt Pindell <pindell.m@gmail.com>
2016-08-06 16:45:21 -0600

    Add customers table

--- sales_admin_submission/db/schema.rb
+++ sales_admin_submission/db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806224007) do
+ActiveRecord::Schema.define(version: 20160806224506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string  "description"
