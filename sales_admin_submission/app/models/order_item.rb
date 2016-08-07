377106017777d00b585d171bbab3d7f6f3e31a3b
Matt Pindell <pindell.m@gmail.com>
2016-08-06 19:05:27 -0600

    Add order_item and corresponding validation/association tests

--- sales_admin_submission/app/models/order_item.rb
+++ sales_admin_submission/app/models/order_item.rb
@@ -1,4 +1,9 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :item
   belongs_to :order
+
+  validates_presence_of :item_id
+  validates_presence_of :order_id
+  validates_presence_of :quantity
+  validates_presence_of :unit_price
 end
