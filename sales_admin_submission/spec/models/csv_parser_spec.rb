620d69b622e93d750bed79d73bb105fe294462e9
Matt Pindell <pindell.m@gmail.com>
2016-08-06 19:48:58 -0600

    Add coverage for customer_parser

--- sales_admin_submission/spec/models/csv_parser_spec.rb
+++ sales_admin_submission/spec/models/csv_parser_spec.rb
@@ -20,4 +20,17 @@ describe CsvParser do
     expect(Order.count).to eq(5)
     expect(OrderItem.count).to eq(5)
   end
+
+  it 'can create customers' do
+    customer_data = { customer_name: "Jane Doe" }
+
+    csv_parser = CsvParser.new
+
+    expect(Customer.count).to eq(0)
+
+    csv_parser.customer_parser(customer_data)
+
+    expect(Customer.count).to eq(1)
+    expect(Customer.first.name).to eq(customer_data[:customer_name])
+  end
 end
