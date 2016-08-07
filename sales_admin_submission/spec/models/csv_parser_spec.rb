a634a28a1718f87897d4bd5fda6d388830026868
Matt Pindell <pindell.m@gmail.com>
2016-08-06 19:59:48 -0600

    Add item_parser coverage

--- sales_admin_submission/spec/models/csv_parser_spec.rb
+++ sales_admin_submission/spec/models/csv_parser_spec.rb
@@ -66,4 +66,23 @@ describe CsvParser do
     expect(Merchant.first.name).to eq(merchant_data[:merchant_name])
     expect(Merchant.first.address).to eq(merchant_data[:merchant_address])
   end
+
+  it 'can create items' do
+    merchant = Merchant.create(name: "Fake Company", address: "1234 Fake St.")
+    item_data = {
+      item_description: "This is a description",
+      item_price: 9.99
+    }
+
+    csv_parser = CsvParser.new
+
+    expect(Item.count).to eq(0)
+
+    csv_parser.item_parser(item_data, merchant.id)
+
+    expect(Item.count).to eq(1)
+    expect(Item.first.description).to eq(item_data[:item_description])
+    expect(Item.first.price).to eq(item_data[:item_price])
+    expect(Item.first.merchant).to eq(merchant)
+  end
 end
